b62606b09f3c912408152b5476e4ab141a36f70d
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-02-11 20:06:57 -0600

    Cart.js, completed readme.md requirements; inc stretch. removed concole.logs f/inside function body and reformated, revised isFull and listItems code

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,26 +1,33 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// Create a global variable named `basket` and set it to an empty array.
 let basket = [];
 const maxItems = 5;
 
 function addItem(item) {
-  while (basket.push(item)) {
-  return true;
- }
+  while (isFull()) {
+    return false;
+  }
+  basket.push(item);
+    return true;
 }
 
-function listItems() {
-  for (i = 0; i < basket.length; i++ ) {
-  console.log ( basket[i] );
+function listItems( array ) {
+  // OR...
+  // for (let i = 0; i < array.length; i++) {
+  // console.log ('Item in basket:', array[i]);
+  i = 0;
+  while (i < array.length) {
+  console.log ('Item in basket:', array[i]);
+  i++;
   }
 }
 
-function empty() {
-  basket.length = 0;
-  // while (basket.length > 0) {
-  //   basket.pop(basket);
-    return 'Empty';
+function empty( array ) {
+  array.length = 0;
+  return array;
   }
 
 function isFull() {
@@ -30,29 +37,51 @@ function isFull() {
     return false;
 }
 
-console.log( 'Basket is:', basket.length );
-console.log( 'Added apples', addItem( 'apples'));
-console.log( 'Basket is now:', basket.length);
+function removeItem(item) {
+  let removedItem;
+  if (basket.indexOf(item) >= 0){
+    removedItem = basket.splice(basket.indexOf(item));
+    return removedItem;
+    }
+      else {
+        return 'null';
+    }
+}
+
+console.log( 'Number of Items in Basket:', basket.length );
+// I created a log called 'Number of Items in Basket' to help me
+//follow along in the console :)
+console.log( 'Adding apple', addItem( 'apple' ));
 console.log( 'Basket is full:', isFull());
-console.log( 'Basket is:', basket.length );
-console.log( 'Added bananas', addItem( 'bananas'));
-console.log( 'Basket is now:', basket.length);
+console.log( 'Number of Items in Basket:', basket.length );
+console.log( listItems(basket));
+console.log( 'Adding banana', addItem( 'banana' ));
 console.log( 'Basket is full:', isFull());
-console.log( 'Basket is:', basket.length );
-console.log( 'Added oranges', addItem( 'oranges'));
-console.log( 'Basket is now:', basket.length);
+console.log( 'Number of Items in Basket:', basket.length );
+console.log( listItems(basket));
+console.log( 'Adding orange', addItem( 'orange' ));
 console.log( 'Basket is full:', isFull());
-console.log( 'Basket is:', basket.length );
-console.log( 'Added limes', addItem( 'limes'));
-console.log( 'Basket is now:', basket.length);
+console.log( 'Number of Items in Basket:', basket.length );
+console.log( listItems(basket));
+console.log( 'Adding lime', addItem( 'lime' ));
 console.log( 'Basket is full:', isFull());
-console.log( 'Basket is:', basket.length );
-console.log( 'Added lemons', addItem( 'lemons'));
-console.log( 'Basket is now:', basket.length);
+console.log( 'Number of Items in Basket:', basket.length );
+console.log( listItems(basket));
+console.log( 'Adding lemon', addItem( 'lemon' ));
 console.log( 'Basket is full:', isFull());
-console.log( 'Basket is:', basket.length );
-console.log( 'Added just one more thing!', addItem( 'one more thing'));
-console.log( 'Basket is now:', basket.length);
+console.log( 'Number of Items in Basket:', basket.length );
+console.log( listItems(basket));
+console.log( 'Adding just one more thing!', addItem( 'one more thing' ));
 console.log( 'Basket is full:', isFull());
-console.log( 'Emptying basket:', empty());
-console.log( 'Basket is:', basket.length );
+console.log( 'Emptying basket:', empty( basket ));
+console.log( 'Adding pear', addItem( 'pear' ));
+console.log( 'Adding plum', addItem( 'plum' ));
+console.log( 'Number of Items in Basket:', basket.length );
+console.log( listItems(basket));
+console.log( 'Basket is full:', isFull());
+console.log( 'Find index number for Plum:', basket.indexOf( 'plum' ));
+console.log( 'Removing Plum:', removeItem( 'plum' ));
+console.log( listItems(basket));
+console.log( 'Number of Items in Basket:', basket.length );
+console.log( listItems(basket));
+console.log( 'Removing Peach (item NOT in basket, should return "Null"):', removeItem( 'peach' ));
